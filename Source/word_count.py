2a3f32f7c334da5db07f15a85b2d9b176c4ce3b7
ArulrajGopal <122370244+ArulrajGopal@users.noreply.github.com>
2024-01-18 11:07:10 +0530

    Create word_count.py

---
+++ Source/word_count.py
@@ -0,0 +1,18 @@
+!pip install pyspark
+
+from pyspark.sql import SparkSession
+
+#create spark session
+spark= SparkSession.builder.appName('mysparksession').getOrCreate()
+
+#create spark context
+sc = spark.sparkContext
+
+
+text_file = sc.textFile('/content/drive/MyDrive/python_home_folder/passage.txt')
+
+text_file = text_file.flatMap(lambda x: x.split(' '))\
+.map(lambda x:(x,1))\
+.reduceByKey(lambda x,y: x+y)
+
+text_file.collect()
